59961223d6c24c3dcdd94fd3498259602881cd7c
Mayu Imanaka <imamayu.1215@gmail.com>
2019-11-06 13:56:52 -0800

    add:settings.js,Board.js,Paddle.js

---
+++ src/partials/Paddle.js
@@ -0,0 +1,26 @@
+import { SVG_NS } from '../settings';
+
+export default class Board {
+    constructor(boardHeight, width, height, x, y) {
+        this.boardHeight = boardHeight;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.speed = 10;
+        this.score = 0;
+    }
+    render(svg) {
+        const paddle = document.createElementNS(SVG_NS, "rect");
+        paddle.setAttributeNS(null, "width", this.width);
+        paddle.setAttributeNS(null, "height", this.height);
+        paddle.setAttributeNS(null, "x", this.x);
+        paddle.setAttributeNS(null, "y", this.y);
+        paddle.setAttributeNS(null, "fill", "#ffffff");
+
+        svg.appendChild(paddle);
+    }
+
+}
+
+
---
+++ src/settings.js
@@ -0,0 +1,10 @@
+export const SVG_NS = "http://www.w3.org/2000/svg";
+export const BOARD_COLOR = "#010101";
+export const BOARD_WIDTH = "512";
+export const BOARD_HEIGHT = "216";
+
+export const PADDLE_WIDTH = "8";
+export const PADDLE_HEIGHT = "56";
+export const PADDLE_GAP = "30";
+
+
