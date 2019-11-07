eb27fbc1fdad9139a369adbddbb771d161439eeb
Mayu Imanaka <imamayu.1215@gmail.com>
2019-11-06 20:28:28 -0800

    update:add ballMove function

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,4 +1,5 @@
 import { SVG_NS } from '../settings';
+import { restElement } from '@babel/types';
 
 export default class Ball {
     constructor(radius, boardWidth, boardHeight) {
@@ -6,16 +7,37 @@ export default class Ball {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         this.direction = 1;
+        this.reset();
+    }
+
+    ballMove() {
+        this.x -= this.vx;
+        this.y += this.vy;
+    }
+    reset() {
+        this.x = this.boardWidth / 2;
+        this.y = this.boardHeight / 2;
+        this.vy = 0;
+        while (this.vy === 0) {
+            this.vy = Math.floor(Math.random() * 10 - 5);
+        }
+        this.vx = this.direction * (6 - Math.abs(this.vy));
+    }
+
+    wallCollision() {
+
     }
 
     render(svg) {
         const circle = document.createElementNS(SVG_NS, "circle");
         circle.setAttributeNS(null, "r", this.radius);
-        circle.setAttributeNS(null, "cx", this.boardWidth);
-        circle.setAttributeNS(null, "cy", this.boardHeight);
+        circle.setAttributeNS(null, "cx", this.x);
+        circle.setAttributeNS(null, "cy", this.y);
         circle.setAttributeNS(null, "fill", "#ffffff");
 
         svg.appendChild(circle);
+        this.ballMove();
+        this.wallCollision();
     }
 }
 
